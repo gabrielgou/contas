d871b7c55a0080c72b903d8329f334ef04fa70db
Gabriel Gouveia <gabriel.gouveia@live.com>
2021-02-11 21:56:35 -0300

    criar no git

---
+++ R/operacoes.R
@@ -0,0 +1,26 @@
+#'Function to math operations
+#'
+#'This function is good to basic math operations.
+#'
+#' @param x A number.
+#' @param y its another number.
+#'
+#' @examples
+#' somar(2,3)
+#'
+#' @export
+somar <- function(x,y){
+  return (x+y)
+}
+#' @export
+subtrair <- function(x,y){
+  return (x-y)
+}
+#' @export
+multiplicar <- function(x,y){
+  return (x*y)
+}
+#' @export
+dividir <- function(x,y){
+  return (x/y)
+}
